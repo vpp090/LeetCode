03ee96f0a3300a11f3d35210fc120c00778bccae
Vasil <vasil.petrov090@gmail.com>
2025-07-23 15:17:41 +0300

    Merge two lists

---
+++ ConsoleApp1/ConsoleApp4/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace MyApp
+{
+    internal class Program
+    {
+
+        public ListNode MergeTwoLists(ListNode list1, ListNode list2)
+        {
+            if (list1 == null) return list2;
+            if (list2 == null) return list1;
+
+            if (list1.val < list2.val)
+            {
+                list1.next = MergeTwoLists(list1.next, list2);
+                return list1;
+            }
+            else
+            {
+                list2.next = MergeTwoLists(list1, list2.next);
+                return list2;
+            }
+        }
+
+        // 1 2 3 4 
+        // 1 6 7 8
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+
+    public class ListNode
+    {
+        public int val;
+        public ListNode next;
+        public ListNode(int val = 0, ListNode next = null)
+        {
+            this.val = val;
+            this.next = next;
+        }
+    }
+
+}
