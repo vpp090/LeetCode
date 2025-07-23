0871c2ef92e372aac7da170bcd3b514a9f2248d8
Vasil <vasil.petrov090@gmail.com>
2025-07-23 14:39:57 +0300

    Reverse linked list

---
+++ ConsoleApp1/ConsoleApp2/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace MyApp
+{
+    internal class Program
+    {
+        public ListNode ReverseList(ListNode head)
+        {
+            ListNode prev = null;
+            ListNode current = head;
+
+            while(current != null)
+            {
+                ListNode nextNode = current.next;
+                current.next = prev;
+                prev = current;
+                current = nextNode;
+            }
+
+            return prev;
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        // null -> 1 -> 2 -> 3 -> 4
+        // null <- 1 <- 2   <-  3
+    }
+
+    public class ListNode
+    {
+        public int val;
+        public ListNode next;
+        public ListNode(int val = 0, ListNode next = null)
+        {
+            this.val = val;
+            this.next = next;
+        }
+    }
+}
