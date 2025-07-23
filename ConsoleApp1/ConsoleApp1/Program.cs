f7166a0488414875f184f11e54c3ab2d53c111de
Vasil <vasil.petrov090@gmail.com>
2025-07-23 14:31:42 +0300

    Two sum problem

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -1,2 +1,32 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace MyApp
+{
+    internal class Program
+    {
+        public int[] TwoSum(int[] nums, int target)
+        {
+            Dictionary<int, int> map = new Dictionary<int, int>();
+
+            for(int i = 0; i < nums.Length; i++)
+            {
+                int complement = target - nums[i];
+                if(map.ContainsKey(complement))
+                {
+                    return [map[complement], i]; 
+                }
+                else
+                {
+                    map[nums[i]] = i;
+                }
+            }
+
+            throw new ArgumentException();
+        }
+       
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
