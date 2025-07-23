3ff30f872d92cf1dcd231ab4c1bd00e2008c2cec
Vasil <vasil.petrov090@gmail.com>
2025-07-23 20:09:01 +0300

    IsPalindrome task

---
+++ ConsoleApp1/ConsoleApp8/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace MyApp
+{
+    internal class Program
+    {
+        public static bool IsPalindrome(string s)
+        {
+            string current = s.ToLower();
+
+            int startIndex = 0;
+            
+            int endIndex = current.Length - 1;
+            while (startIndex <= endIndex)
+            {
+                while (startIndex <= endIndex && !Char.IsAsciiLetterOrDigit(current[startIndex]))
+                    startIndex++;
+                while (startIndex <= endIndex && !Char.IsAsciiLetterOrDigit(current[endIndex]))
+                    endIndex--;
+
+                if (startIndex <= endIndex && current[startIndex] != current[endIndex])
+                    return false;
+
+                startIndex++;
+                endIndex--;
+            }
+
+            return true;
+        }
+        static void Main(string[] args)
+        {
+            var result = IsPalindrome("A man, a plan, a canal: Panama");
+
+            Console.WriteLine(result);
+        }
+    }
+}
---
+++ ConsoleApp1/ConsoleApp9/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace MyApp
+{
+    public class StockPrice
+    {
+
+        public StockPrice()
+        {
+
+        }
+
+        public void Update(int timestamp, int price)
+        {
+
+        }
+
+        public int Current()
+        {
+
+        }
+
+        public int Maximum()
+        {
+
+        }
+
+        public int Minimum()
+        {
+
+        }
+    }
+
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
