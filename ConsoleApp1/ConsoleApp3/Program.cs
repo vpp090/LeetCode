65e09fec72fdfb7993cb791c797d3202430e3a40
Vasil <vasil.petrov090@gmail.com>
2025-07-23 15:03:10 +0300

    Valid parenthesis

---
+++ ConsoleApp1/ConsoleApp3/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace MyApp
+{
+    internal class Program
+    {
+        public static bool IsValid(string s)
+        {
+            Stack<char> stack = new Stack<char>();
+
+            foreach(char c in s)
+            {
+                if(c == '(' || c == '[' || c == '{')
+                {
+                    stack.Push(c);
+                }
+
+                else if(c == ')')
+                {
+                    if (stack.Count == 0 || stack.Pop() != '(') return false;
+                }
+                else if(c == ']')
+                {
+                    if (stack.Count == 0 || stack.Pop() != '[') return false;
+                }
+                else if(c == '}')
+                {
+                    if (stack.Count == 0 || stack.Pop() != '{') return false;
+                }
+
+            }
+
+            return stack.Count == 0;
+        }
+
+
+        static void Main(string[] args)
+        {
+            IsValid("()");
+        }
+    }
+}
