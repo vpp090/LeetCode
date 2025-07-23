34f40f31e2064b200b9cd014a43b76659420b6a1
Vasil <vasil.petrov090@gmail.com>
2025-07-23 17:30:36 +0300

    BInarySearch and ClimbingStairs

---
+++ ConsoleApp1/ConsoleApp5/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace MyApp
+{
+    internal class Program
+    {
+        public int Search(int[] nums, int target)
+        {
+            int low = 0;
+            int high = nums.Length - 1;
+
+            return BinarySearch(nums, low, high, target);
+
+        }
+
+        public int BinarySearch(int[] nums, int low, int high, int target)
+        {
+            if (low > high)
+                return -1;
+
+            int mid = low + (high - low) / 2;
+
+
+            if (nums[mid] == target)
+                return mid;
+            else if (target > nums[mid])
+            {
+                mid = BinarySearch(nums, mid + 1, high, target);
+            }
+            else
+            {
+                mid = BinarySearch(nums, low, mid - 1, target);
+            }
+
+            return mid;
+        }
+            static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ ConsoleApp1/ConsoleApp6/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace MyApp
+{
+    internal class Program
+    {
+        public int ClimbStairs(int n)
+        {
+            if (n <= 2) return n;
+
+            int oneStepBefore = 2;
+            int twoStepBefore = 1;
+            int allWays = 0;
+
+            for(int i = 3; i <= n; i++)
+            {
+                allWays = oneStepBefore + twoStepBefore;
+                twoStepBefore = oneStepBefore;
+                oneStepBefore = allWays;
+            }
+
+            return allWays;
+        }
+
+        // 1, 2 = 1 + 1 / 2, 3 = (2) + (1)
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
