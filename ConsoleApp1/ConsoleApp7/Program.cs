a7ba2f0a008fa05256aca495fe4d9f243a24b9d4
Vasil <vasil.petrov090@gmail.com>
2025-07-23 18:00:48 +0300

    Best time to sell stock

---
+++ ConsoleApp1/ConsoleApp7/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace MyApp
+{
+    internal class Program
+    {
+        public int MaxProfit(int[] prices)
+        {
+            int currentMin = prices[0];
+            int maxProfit = 0;
+
+            for(int i =0;  i < prices.Length; i++)
+            {
+                if(prices[i] < currentMin)
+                {
+                    currentMin = prices[i];
+                }
+                else if(prices[i] - currentMin > maxProfit)
+                {
+                    maxProfit = prices[i] - currentMin;
+                }
+            }
+
+            return maxProfit;
+        }
+
+        
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
